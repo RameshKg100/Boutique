fe408f1e5bb392e5d11be20ad1045c03cab72587
Ramesh R <ddramesh24@gmail.com>
2026-05-03 13:02:44 +0530

    Remove Facebook link and enhance WhatsApp icon visibility

--- src/data/siteConfig.js
+++ src/data/siteConfig.js
@@ -11,7 +11,6 @@ export const siteConfig = {
   },
   social: {
     instagram: "https://www.instagram.com/sathyas__boutique?igsh=Mjg3YXZqd2hpNGlp",
-    facebook: "https://facebook.com/sathyasboutique",
     youtube: "https://youtube.com/@sathyasboutique",
   },
   mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3912.8739322877336!2d77.09505597590082!3d11.27067638890974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba8e5007c85f95b%3A0xf3a9141ab88213d6!2sSathyas%20Boutique!5e0!3m2!1sen!2sin!4v1777559084493!5m2!1sen!2sin",
